0f318b974c53629c9375d239679019ca83ce8434
Cory Tveten <corytveten@gmail.com>
2021-06-11 22:23:22 -0400

    begin component structure

--- trivia-app-frontend/src/App.js
+++ trivia-app-frontend/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import QuestionListContainer from './containers/QuestionListContainer';
+
 function App() {
   return (
     <div className="App">
       <h1>TRIVIA APP</h1>
+      < QuestionListContainer />
     </div>
   );
 }
---
+++ trivia-app-frontend/src/containers/QuestionListContainer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class QuestionListContainer extends Component {
+
+    render() {
+        return (
+            <p>question list container</p>
+        )
+    }
+
+    //fetch questions methods
+
+    //componentDidMount method which will call fetch questions
+}
+
+export default QuestionListContainer
