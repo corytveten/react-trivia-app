4dafc1c795728b64079aaa2fb62da9e6d8fb6fc6
Cory Tveten <corytveten@gmail.com>
2021-06-13 22:09:32 -0400

    react router setup

--- trivia-app-frontend/src/App.js
+++ trivia-app-frontend/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './components/Home';
 import QuestionListContainer from './containers/QuestionListContainer';
 
 function App() {
   return (
-    <div className="App" className='container'>
-      <h1>TRIVIA APP</h1>
-      < QuestionListContainer />
+    <div className="App">
+      <Router>
+        <Route path={'/questions'} component={QuestionListContainer}></Route>
+        <Route path={'/'} component={Home}></Route>
+      </Router>
+      {/* <h1>TRIVIA APP</h1> */}
+      {/* < QuestionListContainer /> */}
     </div>
   );
 }
