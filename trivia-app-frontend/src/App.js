77b524c0a719512360b4cd69db743c01f461939d
Cory Tveten <corytveten@gmail.com>
2021-06-13 22:29:57 -0400

    add navbar

--- trivia-app-frontend/src/App.js
+++ trivia-app-frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from './components/Navbar'
 import Home from './components/Home';
 import QuestionListContainer from './containers/QuestionListContainer';
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <Route path={'/questions'} component={QuestionListContainer}></Route>
         <Route path={'/'} component={Home}></Route>
       </Router>
