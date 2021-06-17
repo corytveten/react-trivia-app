50e4b53503de6dbb77d2e93bbd2ee8ee53a6bfec
Cory Tveten <corytveten@gmail.com>
2021-06-17 16:34:47 -0400

    add handle methods to login form

--- trivia-app-frontend/src/components/Login.js
+++ trivia-app-frontend/src/components/Login.js
@@ -1,16 +1,47 @@
 import React from 'react';
  
 class Login extends React.Component {
+
+  state = {
+    username: '',
+    password: ''
+  }
+
+  handleUsernameChange = event => {
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  handlePasswordChange = event => {
+    this.setState({
+      password: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    let formData = { username: this.state.username, password: this.state.password }
+    console.log(formData)
+    // fetch('http://localhost:4000/users', {
+    //   method: "POST",
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   },
+    //   body: JSON.stringify(formData)
+    // })
+  }
+
   render() {
     return (
-      <form className='container'>
+      <form className='container' onSubmit={event => this.handleSubmit(event)}>
         <h1>Login</h1>
         <div>
-          <input type="text" name="username" placeholder="Username" />
+          <input type="text" name="username" placeholder="Username" onChange={event => this.handleUsernameChange(event)} value={this.state.username}/>
           <label htmlFor="username">Username</label>
         </div>
         <div>
-          <input type="password" name="password" placeholder="Password" />
+          <input type="password" name="password" placeholder="Password" onChange={event => this.handlePasswordChange(event)} value={this.state.password}/>
           <label htmlFor="password">Password</label>
         </div>
         <input type="submit" value="Login" />
