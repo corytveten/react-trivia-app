745e729e7b58edfa77518133b36cd83929412e05
Cory Tveten <corytveten@gmail.com>
2021-06-17 16:24:28 -0400

    add state to signup

--- trivia-app-frontend/src/components/Signup.js
+++ trivia-app-frontend/src/components/Signup.js
@@ -1,16 +1,33 @@
 import React from 'react';
  
 class Signup extends React.Component {
+  state = {
+    username: '',
+    password: ''
+  }
+
+  handleUsernameChange = event => {
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  handlePasswordChange = event => {
+    this.setState({
+      password: event.target.value
+    })
+  }
+
   render() {
     return (
       <form className='container'>
         <h1>Create an account</h1>
         <div>
-          <input type="text" name="username" placeholder="Username" />
+          <input type="text" name="username" placeholder="Username" onChange={event => this.handleUsernameChange(event)} value={this.state.username}/>
           <label htmlFor="username">Username</label>
         </div>
         <div>
-          <input type="password" name="password" placeholder="Password" />
+          <input type="password" name="password" placeholder="Password" onChange={event => this.handlePasswordChange(event)} value={this.state.password}/>
           <label htmlFor="password">Password</label>
         </div>
         <input type="submit" value="Login" />
