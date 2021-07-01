52c6e705ff6efc261a4b3a2db9e80b2235688877
Cory Tveten <corytveten@gmail.com>
2021-07-01 13:49:48 -0400

    clear comments

--- trivia-app-frontend/src/App.js
+++ trivia-app-frontend/src/App.js
@@ -3,14 +3,12 @@ import './App.css';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './components/Navbar'
-// import Home from './components/Home';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import UserPage from './components/UserPage'
 import QuestionListContainer from './containers/QuestionListContainer';
-// import UserContainer from './containers/UserContainer';
 import Home from './components/Home';
-// import UserContainer from './containers/UserContainer';
+
 
 function App() {
   return (
@@ -21,11 +19,8 @@ function App() {
         <Route exact path={'/login'} component={Login}></Route>
         <Route exact path={'/signup'} component={Signup}></Route>
         <Route exact path={'/user'} component={UserPage}></Route>
-        {/* <Route path={'/'} component={Home}></Route> */}
         <Route exact path={'/'} component={Home}></Route>
       </Router>
-      {/* <h1>TRIVIA APP</h1> */}
-      {/* < QuestionListContainer /> */}
     </div>
   );
 }
--- trivia-app-frontend/src/index.js
+++ trivia-app-frontend/src/index.js
@@ -4,10 +4,8 @@ import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-// import SignUpReducer from './reducers/SignUpReducer'
 import questionsReducer from './reducers/questionsReducer';
 import UsersReducer from './reducers/UsersReducer';
-// import LoginReducer from './reducers/LoginReducer'
 import './index.css';
 import App from './App';
 
@@ -16,18 +14,12 @@ const composedEnhancer = compose(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
 const reducers = combineReducers({
-  // SignUpReducer,
   questionsReducer,
   UsersReducer
 })
 
 const store = createStore(reducers, composedEnhancer)
 
-// const store = createStore( 
-//   SignUpReducer,
-//   applyMiddleware(thunk),
-//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-//   );
 
 ReactDOM.render(
   <Provider store={store}>
